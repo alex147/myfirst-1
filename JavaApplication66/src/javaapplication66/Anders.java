90b9043df398ceef3ee3d7c01f6d1c5f0814354c
Li <Li@10.154.1.65>
2013-02-26 11:20:50 +0100

    hej

--- JavaApplication66/src/javaapplication66/Anders.java
+++ JavaApplication66/src/javaapplication66/Anders.java
@@ -4,10 +4,19 @@
  */
 package javaapplication66;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author andersb
  */
 public class Anders {
+
     int i = 45;
+
+    public Anders() {
+        while (true) {
+            JOptionPane.showMessageDialog(null, "Anders is the KING");
+        }
+    }
 }
--- JavaApplication66/src/javaapplication66/JavaApplication66.java
+++ JavaApplication66/src/javaapplication66/JavaApplication66.java
@@ -1,5 +1,7 @@
 package javaapplication66;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author andersb
@@ -9,12 +11,16 @@ public class JavaApplication66 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {        
-        System.out.println("Hello world");
-        System.out.println("Another message");
-        System.out.println("Yet another message");
-        System.out.println("More");
-        System.out.println("even more");
-        System.out.println("anders wrote this");
+    public static void main(String[] args) {       
+      
+        Anders s = new Anders();
+        
+      
+//        System.out.println("Hello world");
+//        System.out.println("Another message");
+//        System.out.println("Yet another message");
+//        System.out.println("More");
+//        System.out.println("even more");
+//        System.out.println("anders wrote this");
     }
 }
